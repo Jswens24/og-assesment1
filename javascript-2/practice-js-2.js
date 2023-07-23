5b0239f34cf336e9785410a290c7a62adaa110ca
Jessica Swenson <jessicamswens@gmail.com>
2023-07-23 12:27:23 -0600

    added part 2 of assessment

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+const gitDefinition = 'Git is a version control system';
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,47 +17,59 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+const gitHubDefinition = 'GitHub is a place to externally store your repositories'
 
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
-    For the next several problems you will be creating objects containing information about different git 
-    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
-    will be a string with a description of what that git command does.  The 'code' property should be a 
+    For the next several problems you will be creating objects containing information about different git
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property
+    will be a string with a description of what that git command does.  The 'code' property should be a
     string of the actual command (what you type into your terminal).
 */
 
 //////////////////PROBLEM 3////////////////////
 /*
-    Create an object called 'init' with 'description' and 'code' properties 
+    Create an object called 'init' with 'description' and 'code' properties
     following the guidelines above to describe the init command.
 */
 
-//CODE HERE
+const init = {
+    'description': 'starts tracking versions of your project',
+    'code': 'git init'
+}
 
 //////////////////PROBLEM 4////////////////////
 /*
-    Create an object called 'clone' with 'description' and 'code' properties 
+    Create an object called 'clone' with 'description' and 'code' properties
     following the guidelines above to describe the clone command.
 */
 
-//CODE HERE
+const clone = {
+    'description': 'copies an existing project',
+    'code': 'git clone https://someproject.com'
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
-    Create an object called 'status' with 'description' and 'code' properties 
+    Create an object called 'status' with 'description' and 'code' properties
     following the guidelines above to describe the status command.
 */
 
-//CODE HERE
+const status = {
+    'description': 'tells you what branch you are working in and the status of your files',
+    'code': 'git status'
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
-    Create an object called 'add' with 'description' and 'code' properties 
+    Create an object called 'add' with 'description' and 'code' properties
     following the guidelines above to describe the add command.
 */
 
-//CODE HERE
+const add = {
+    'description': 'adds files that have changes to queue for next commit',
+    'code': 'git add .'
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +77,10 @@
     following the guidelines above to describe the commit command.
 */
 
-//CODE HERE
+const commit = {
+    'description': 'creates a snapshot of the files that are being tracked',
+    'code': "git commit -m 'what i updated' "
+}
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,12 +88,18 @@
     following the guidelines above to describe the command to add a remote location to your git repository.
 */
 
-//CODE HERE
+const addRemote = {
+    'description': 'the adds your git repo and an external location',
+    'code': 'git remote add origin https://github.com/Jswens24/og-assesment132.git'
+}
 
 //////////////////PROBLEM 9////////////////////
 /*
-    Create an object called 'push' with 'description' and 'code' properties 
+    Create an object called 'push' with 'description' and 'code' properties
     following the guidelines above to describe the push command.
 */
 
-//CODE HERE
+const push = {
+    'description': 'brings last commit to remote repo',
+    'code': 'git push'
+}
